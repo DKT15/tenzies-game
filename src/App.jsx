22a4ad90b05b48e0df336053a050c573df3814b4
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-10-05 21:19:30 +0100

    added to the code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
 import React from "react";
 import "./App.css";
+import Die from "./Die";
+import { nanoid } from "nanoid";
 
 function App() {
+  //
+  const [dice, setDice] = React.useState(allNewDice());
+  // loops 10 times throught numbers 1 to 6 and pushes the results to the newDice array.
+  // isHeld is automatically set to false. When the user clicks it will turn green and true.
+  // random id is created for each element and is set to the key of the die.
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
+    }
+    return newDice;
+  }
+
+  // clicking the RollDice button will get a new set of ten numbers and set the dice set to the new array.
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  const diceElements = dice.map((die) => (
+    <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+  ));
+
   return (
-    <>
-      <main>
-        <div className="dice-container">
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-        </div>
-      </main>
-    </>
+    <main>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        Roll
+      </button>
+    </main>
   );
 }
 
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "../styles/Die.css";
 
-export default function Die(props) {
+function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white", //if true, it will be green, if false it will be white.
+  };
   return (
-    <div className="die-face">
+    <div className="die-face" style={styles}>
       <h2 className="die-num">{props.value}</h2>
     </div>
   );
 }
+
+export default Die;
