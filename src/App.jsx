fe6d1512aca33e0019b346631d4f7ff30f8d0a2d
Danté Kellman-Thompson <dante.kellman@icloud.com>
2024-10-06 12:30:47 +0100

    added to the code.

--- src/App.jsx
+++ src/App.jsx
@@ -4,34 +4,70 @@ import Die from "./Die";
 import { nanoid } from "nanoid";
 
 function App() {
-  //
+  // initialising allNewDice in useState, so it displays when the app loads.
   const [dice, setDice] = React.useState(allNewDice());
-  // loops 10 times throught numbers 1 to 6 and pushes the results to the newDice array.
+
+  // Helper function that is used in appropriate places in the code.
+  // value gets a random number between 1 and 6.
   // isHeld is automatically set to false. When the user clicks it will turn green and true.
   // random id is created for each element and is set to the key of the die.
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
+  // loops 10 times throught numbers 1 to 6 and pushes the results to the newDice array and pushes generateNewDice when the game starts.
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid(),
-      });
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
 
-  // clicking the RollDice button will get a new set of ten numbers and set the dice set to the new array.
+  // when rollDice is clicked, it looks through the existing roll. If the die is being held,
+  // it is kept in the array but if it is not held then a new dice is generated to be added to the array.
   function rollDice() {
-    setDice(allNewDice());
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
   }
 
+  // when clicked, flips the isHeld property based on the id.
+  // If it is the same die with the id property that was passed into the function, the object will be updated and if not die will be returned. WHich is keeping the same object that was there before.
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die; //if the id's are equal. I take all of the properties from the original die and set the isHeld to be the opposite of die.isHeld bacause it is defaulted to false.
+      })
+    );
+  }
+
+  // map through each die and setting it an id, value (passes down the value to props in Die.jsx)
+  // isHeld property which is defaulted as false.
+  // when a dice is clicked holdDice will give it its own unique id.
   const diceElements = dice.map((die) => (
-    <Die key={die.id} value={die.value} isHeld={die.isHeld} />
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      holdDice={() => holdDice(die.id)}
+    />
   ));
 
+  //adding each diceElement to the container.
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{diceElements}</div>
       <button className="roll-dice" onClick={rollDice}>
         Roll
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -5,8 +5,11 @@ function Die(props) {
   const styles = {
     backgroundColor: props.isHeld ? "#59E391" : "white", //if true, it will be green, if false it will be white.
   };
+  //adding the styling to each dice face. The styling will be green if true and white if false.
+  // props.vale links up to the die value within App.jsx.
+  // holdDice assigns an id to each dice when it is clicked. Props is passing it down from the App.jsx.
   return (
-    <div className="die-face" style={styles}>
+    <div className="die-face" onClick={props.holdDice} style={styles}>
       <h2 className="die-num">{props.value}</h2>
     </div>
   );
